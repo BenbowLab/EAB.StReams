b8bf8ca5c36b68d21ba3ddd060f644ec575c223d
Courtney Larson <larso126@msu.edu>
2017-10-31 19:36:58 -0400

    Create Ent Soc Figures

--- Riparian_Woody_Debris.R
+++ Riparian_Woody_Debris.R
@@ -1,1 +1,68 @@
+# library
+library(ggplot2)
+
+#Functions
+## Summarizes data.
+## Gives count, mean, standard deviation, standard error of the mean, and confidence 
+## interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE, conf.interval=.95) {
+  library(doBy)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # Collapse the data
+  formula <- as.formula(paste(measurevar, paste(groupvars, collapse=" + "), sep=" ~ "))
+  datac <- summaryBy(formula, data=data, FUN=c(length2,mean,sd), na.rm=na.rm)
+  
+  # Rename columns
+  names(datac)[ names(datac) == paste(measurevar, ".mean",    sep="") ] <- measurevar
+  names(datac)[ names(datac) == paste(measurevar, ".sd",      sep="") ] <- "sd"
+  names(datac)[ names(datac) == paste(measurevar, ".length2", sep="") ] <- "N"
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
 #Upload Woody debris riparian count data
+EAB_Rip_WD<-read.csv("~/Documents/MSU/Research/Surveying/Riparian/Riparian_Woody_Debris.csv", sep = ",", header = T )
+#make percent ash add to 1
+EAB_Rip_WD$Percent_ash<-EAB_Rip_WD$Percent_ash/100
+#visualize differences between forest and gap overall
+ggplot(EAB_Rip_WD, aes(x=Stream, y=Percent_ash, fill=Location)) + 
+  geom_boxplot() +
+  ylab("Percent Ash") +
+  xlab("Watershed")+
+  scale_colour_hue(name="Gap Location")+
+  theme_classic()+
+  theme(axis.title.x=element_text(size=20),axis.title.y=element_text(size=20),
+        axis.text.x=element_text(size=14),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=20),legend.text = element_text(size=16))
+
+#check for normality
+shapiro.test(EAB_Rip_WD$Percent_ash)
+#not normal
+kruskal.test(Percent_ash ~ Stream, EAB_Rip_WD)
+#Identical populations
+
+
+#Paired t test
+T_EAB_Rip<- cast(EAB_Rip_WD, Watershed ~ Location, value = "Percent_ash")
+t.test(T_EAB_Rip$Forest,T_EAB_Rip$Gap,paired=T)
+
+
--- Woody_Debris.R
+++ Woody_Debris.R
@@ -2,6 +2,7 @@
 
 #Upload libraries
 library(ggplot2)
+library(lme4)
 #Upload data
 EAB_LWD<-read.csv("~/Documents/MSU/Research/Surveying/CWD_Survey.csv", sep=",", header = T)
 #Create new variable for location
@@ -14,38 +15,109 @@ EAB_LWD$TWD_Density<-as.numeric(EAB_LWD$Total_LWD/EAB_LWD$Transect_Length_m)
 EAB_LWD$AWD_Density<-as.numeric(EAB_LWD$Number_Ash_LWD/EAB_LWD$Transect_Length_m)
 #Create new variable for ratio of ash to total woody debris
 EAB_LWD$Ash_Ratio<-as.numeric(EAB_LWD$Number_Ash_LWD/EAB_LWD$Total_LWD)
+#order for upstream, gap, downstream
+str(EAB_LWD)
+EAB_LWD$Gap_location<- factor(EAB_LWD$Gap_location, levels=c("Upstream", "Gap", "Downstream"))
+
+#Plots for percent ash
+#Create line plots
+ggplot(EAB_LWD, aes(x=Watershed, y=Ash_Ratio, colour=Gap_location)) + 
+  geom_boxplot() +
+  ylab("Percent Ash") +
+  scale_colour_hue(name="Gap Location")+
+  theme_classic()+
+  theme(axis.title.x=element_text(size=20),axis.title.y=element_text(size=20),
+        axis.text.x=element_text(size=14),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=20),legend.text = element_text(size=16))
 
 #Create heat maps
-#Base plot
-TWD<-ggplot(EAB_LWD, aes(x=Stream_name, y=LocationID, fill=TWD_Density))
 #using geom_tile
-TWD+
-  geom_tile()+
-  xlab("Stream name")+
-  scale_x_discrete(limits=c("Augusta Creek", "Seven Mile Creek", "Sessions Creek", "Frayer Creek", "Spring Creek"))+
-  ylab("Gap Location")+
-  labs(fill="Woody Debris Density")+
-  scale_fill_gradient(low="#f7fcf5", high="#00441b")
-
 #Base plot
-AWD<-ggplot(EAB_LWD, aes(x=Stream_name, y=LocationID, fill=AWD_Density))
-#using geom_tile
+AWD<-ggplot(EAB_LWD, aes(x=Watershed, y=Gap_location, fill=Ash_Ratio))
 AWD+
   geom_tile()+
-  xlab("Stream name")+
-  scale_x_discrete(limits=c("Augusta Creek", "Seven Mile Creek", "Sessions Creek", "Frayer Creek", "Spring Creek"))+
+  xlab("Watershed")+
+  scale_x_discrete(limits=c("Kalamazoo", "Grand", "Clinton"))+
   ylab("Gap Location")+
-  labs(fill="Ash WD Density")+
-  scale_fill_gradient(low="#f7fcf5", high="#00441b")
+  labs(fill="Percent Ash
+Woody Debris")+
+  scale_fill_gradient(low="#f7fcf5", high="#00441b")+
+  theme_classic()+
+  theme(axis.title = element_text(size = rel(2)))+
+  theme(axis.text = element_text(size = rel(1.5)))+
+  theme(legend.title = element_text(size=rel(1.5)))+
+  theme(legend.text = element_text(size=rel(1)))
 
-#Base plot
-DL<-ggplot(EAB_LWD, aes(x=Stream_name, y=LocationID, fill=Average_Decay_Level))
-#using geom_tile
-DL+
-  geom_tile()+
-  xlab("Stream name")+
-  scale_x_discrete(limits=c("Augusta Creek", "Seven Mile Creek", "Sessions Creek", "Frayer Creek", "Spring Creek"))+
-  ylab("Gap Location")+
-  labs(fill="Average Decay Level")+
-  scale_fill_gradient(low="#f7fcf5", high="#00441b")
+#Stats for percent ash in stream
+
+#test for normality of distribution
+shapiro.test(EAB_LWD$Ash_Ratio)
+#We reject the null hypothesis and determine not normally distributed
+#test different values for watersheds
+kruskal.test(Ash_Ratio ~ Watershed, EAB_LWD)
+#We reject the null hypothesis and determine each watershed has different ash ratios. 
+#Therefore use each watershed for further tests
+#subset dataset by each watershed
+EAB_LWD_KZ<-subset(EAB_LWD, Watershed=="Kalamazoo")
+EAB_LWD_GR<-subset(EAB_LWD, Watershed=="Grand")
+EAB_LWD_CL<-subset(EAB_LWD, Watershed=="Clinton")
+#Determine if differences for each stream in each watershed
+shapiro.test(EAB_LWD_KZ$Ash_Ratio)
+#Kalamazoo not normal
+shapiro.test(EAB_LWD_GR$Ash_Ratio)
+#Grand not normal
+shapiro.test(EAB_LWD_CL$Ash_Ratio)
+#Clinton normal
+wilcox.test(Ash_Ratio ~ Stream_name, EAB_LWD_KZ)
+#We fail to reject the null hypothesis, do not have different ratios
+wilcox.test(Ash_Ratio ~ Stream_name, EAB_LWD_GR)
+#We fail to reject the null hypothesis, do not have different ratios
+#Only ony stream in Clinton watershed
+#We do not need to run different tests for each stream
+EAB_LWD_KZ$Block<-factor(paste(EAB_LWD_KZ$Stream_name,EAB_LWD_KZ$Gap_number))
+friedman.test(EAB_LWD_KZ$Ash_Ratio, EAB_LWD_KZ$Gap_location, EAB_LWD_KZ$Block)
+#Not significant
+EAB_LWD_GR$Block<-factor(paste(EAB_LWD_GR$Stream_name,EAB_LWD_GR$Gap_number))
+friedman.test(EAB_LWD_GR$Ash_Ratio, EAB_LWD_GR$Gap_location, EAB_LWD_GR$Block)
+#Not significant
+EAB_LWD_CL$Block<-factor(paste(EAB_LWD_CL$Stream_name,EAB_LWD_CL$Gap_number))
+anova(lm(Ash_Ratio ~ Gap_location + Block, EAB_LWD_CL))
+#Not significant
+
+#Now look at decay levels
+ggplot(EAB_LWD, aes(x=Watershed, y=Average_Decay_Level, colour=Gap_location)) + 
+  geom_boxplot() +
+  ylab("Decay Level") +
+  scale_colour_hue(name="Gap Location")+
+  theme_classic()+
+  theme(axis.title.x=element_text(size=20),axis.title.y=element_text(size=20),
+        axis.text.x=element_text(size=14),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=20),legend.text = element_text(size=16))
+
+#Stats for decay level
+#test for normality of distribution
+shapiro.test(EAB_LWD$Average_Decay_Level)
+#Not normal
+#test different values for watersheds
+kruskal.test(Average_Decay_Level ~ Watershed, EAB_LWD)
+#not identical, so split up by watershed
+#Determine if differences for each stream in each watershed
+shapiro.test(EAB_LWD_KZ$Average_Decay_Level)
+#Kalamazoo normal
+shapiro.test(EAB_LWD_GR$Average_Decay_Level)
+#Grand not normal
+shapiro.test(EAB_LWD_CL$Average_Decay_Level)
+#Clinton normal
+t.test(EAB_LWD_KZ$Average_Decay_Level ~ EAB_LWD_KZ$Stream_name)
+#no difference in streams
+wilcox.test(Average_Decay_Level ~ Stream_name, EAB_LWD_GR)
+#no difference in streams
+#clinton only one stream
+#Continue analyses within each watershed
+friedman.test(EAB_LWD_KZ$Average_Decay_Level, EAB_LWD_KZ$Gap_location, EAB_LWD_KZ$Block)
+#Not significant
+friedman.test(EAB_LWD_GR$Average_Decay_Level, EAB_LWD_GR$Gap_location, EAB_LWD_GR$Block)
+#Not significant
+friedman.test(EAB_LWD_CL$Average_Decay_Level, EAB_LWD_CL$Gap_location, EAB_LWD_CL$Block)
+#Not significant
 
