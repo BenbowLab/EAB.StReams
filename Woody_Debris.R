bd10424eae1b5069cf0fa6786188588886e77bcc
Courtney Larson <larso126@msu.edu>
2017-11-02 11:51:50 -0400

    Further Analysis with Alberto

--- Woody_Debris.R
+++ Woody_Debris.R
@@ -18,6 +18,8 @@ EAB_LWD$TWD_Density<-as.numeric(EAB_LWD$Total_LWD/EAB_LWD$Transect_Length_m)
 EAB_LWD$AWD_Density<-as.numeric(EAB_LWD$Number_Ash_LWD/EAB_LWD$Transect_Length_m)
 #Create new variable for ratio of ash to total woody debris
 EAB_LWD$Ash_Ratio<-as.numeric(EAB_LWD$Number_Ash_LWD/EAB_LWD$Total_LWD)
+#Create new variable for ash density percent
+EAB_LWD$AshPer<-as.numeric(EAB_LWD$AWD_Density/EAB_LWD$TWD_Density)
 #order for upstream, gap, downstream
 str(EAB_LWD)
 EAB_LWD$Gap_location<- factor(EAB_LWD$Gap_location, levels=c("Upstream", "Gap", "Downstream"))
@@ -35,6 +37,15 @@ ggplot(EAB_LWD, aes(x=Gap_location, y=Ash_Ratio, colour=Watershed)) +
         axis.text.x=element_text(size=14),axis.text.y = element_text(size=14),
         legend.title=element_text(size=20),legend.text = element_text(size=16))+
   scale_color_manual(values=wes_palette(n=3, name="GrandBudapest"))
+
+#Create line plots
+ggplot(EAB_LWD, aes(x=Gap_location, y=Ash_Ratio)) + 
+  geom_boxplot() +
+  ylab("Percent Ash") +
+  xlab("Gap Location")+
+  theme_classic()+
+  theme(axis.title.x=element_text(size=20),axis.title.y=element_text(size=20),
+        axis.text.x=element_text(size=14),axis.text.y = element_text(size=14))
 
 #Create heat maps
 #using geom_tile
@@ -57,9 +68,35 @@ Woody Debris")+
 #Stats for percent ash in stream
 
 #test for normality of distribution
-shapiro.test(EAB_LWD$Ash_Ratio)
+shapiro.test(EAB_LWD$AshPer)
 #We reject the null hypothesis and determine not normally distributed
-anova(lmer(SR_Ash_Ratio ~ Gap_location + (1|Gap_number/Stream_name), EAB_LWD))
+#Transform arcsin function
+EAB_LWD$arcsinAshPer<-asin(EAB_LWD$AshPer)
+shapiro.test(EAB_LWD$arcsinAshPer)
+#test for outliers
+dotchart(EAB_LWD$arcsinAshPer)
+#appears to be one outlier
+plot(EAB_LWD$arcsinAsh_Ratio)
+identify(EAB_LWD$arcsinAsh_Ratio)
+#Create new data frame with outlier deleted (augusta creek gap 2 downstream)
+EAB_LWD_NO<-EAB_LWD[-c(12),]
+
+AR<-glmer(Ash_Ratio ~ Gap_location + Watershed + Gap_number + (1|Stream_name), family=binomial, data = EAB_LWD_NO)
+summary(AR)
+#no significance, possibly too few data points
+
+drop1(AR)
+#run model again without gap_number
+AR1<-glmer(Ash_Ratio ~ Gap_location + Watershed + (1|Stream_name), family=binomial, data = EAB_LWD_NO)
+summary(AR1)
+
+drop1(AR1)
+
+#try model with transformed data
+ART<-lm(arcsinAsh_Ratio ~ Gap_location + Watershed, data = EAB_LWD_NO)
+summary(ART)
+alias(ART)
+anova(lmer(arcsinAsh_Ratio ~ Gap_location + (1|Gap_number/Stream_name), EAB_LWD))
 
 #Now look at decay levels
 ggplot(EAB_LWD, aes(x=Gap_location, y=Average_Decay_Level, colour=Watershed)) + 
@@ -77,3 +114,17 @@ ggplot(EAB_LWD, aes(x=Gap_location, y=Average_Decay_Level, colour=Watershed)) +
 shapiro.test(EAB_LWD$Average_Decay_Level)
 #Not normal
 anova(lmer(Average_Decay_Level ~ Gap_location + (1|Gap_number/Stream_name), EAB_LWD))
+
+#analyze ash density
+#Create line plots
+anova(lmer(AWD_Density ~ Gap_location + (1|Gap_number/Stream_name), EAB_LWD))
+ggplot(EAB_LWD, aes(x=Gap_location, y=AWD_Density, colour=Watershed)) + 
+  geom_boxplot() +
+  ylab("Ash Density") +
+  xlab("Gap Location")+
+  theme_classic()+
+  theme(axis.title.x=element_text(size=20),axis.title.y=element_text(size=20),
+        axis.text.x=element_text(size=14),axis.text.y = element_text(size=14),
+        legend.title=element_text(size=20),legend.text = element_text(size=16))+
+  scale_color_manual(values=wes_palette(n=3, name="GrandBudapest"))
+
